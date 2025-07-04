25a09a8be3ac17534a4cdb2ab543acd07bafb57d
zhiwei <1178679449@qq.com>
2025-07-04 17:25:01 +0800

    内存修改

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ logging.getLogger('werkzeug').disabled = True
 
 os.environ["PADDLE_PDX_CACHE_HOME"] = "./module"
 os.environ["PADDLE_PDX_LOCAL_FONT_FILE_PATH"] = "./module/simfang.ttf"
+os.environ["FLAGS_allocator_strategy"] = "naive_best_fit"
 
 paddleocr = PaddleOCR(
     use_doc_orientation_classify=False,
