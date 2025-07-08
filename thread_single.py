3b6ffee9bcee3c6c4a8874db155f476c8c1449e6
zhiwei <1178679449@qq.com>
2025-07-08 17:40:11 +0800

    订单号解析修改

--- thread_single.py
+++ thread_single.py
@@ -8,7 +8,7 @@ from concurrent.futures import ThreadPoolExecutor
 class PaddleOCRModelManager(ThreadPoolExecutor):
 
     def __init__(self,current_app, **kwargs):
-        super(PaddleOCRModelManager, self).__init__(max_workers= 1,thread_name_prefix="test_",**kwargs)
+        super(PaddleOCRModelManager, self).__init__(max_workers= 2,thread_name_prefix="test_",**kwargs)
         os.environ["PADDLE_PDX_CACHE_HOME"] = "./module"
         # os.environ["PADDLE_PDX_LOCAL_FONT_FILE_PATH"] = "./module/simfang.ttf"
         # os.environ["FLAGS_allocator_strategy"] = "naive_best_fit"
@@ -33,7 +33,7 @@ class PaddleOCRModelManager(ThreadPoolExecutor):
         for res in result:
             order_exist = False
             for text in res['rec_texts']:
-                if "订单号" in text or "流水" in text or "小票号" in text or "单据号" in text or "No." in text:
+                if "订单号" in text or "流水" in text or "小票号" in text or "单据号" in text or "No." in text or "订单编号" in text:
                     self.app.logger.info(text)
                     order_exist=True
             if not order_exist:
