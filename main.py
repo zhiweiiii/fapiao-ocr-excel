15596ab2e7f376252670d11f25dab6c29e3053b4
zhiwei <1178679449@qq.com>
2025-07-08 17:19:21 +0800

    单线程修改

--- main.py
+++ main.py
@@ -1,33 +1,16 @@
 import logging
-import os
-import time
-
-
 
 import numpy
 from PIL import Image
-from paddleocr import PaddleOCR
 
-from flask import Flask, request, make_response,abort
+from flask import Flask, request
 import json
 
+from thread_single import PaddleOCRModelManager
 
 logging.getLogger('werkzeug').disabled = True
-
-os.environ["PADDLE_PDX_CACHE_HOME"] = "./module"
-# os.environ["PADDLE_PDX_LOCAL_FONT_FILE_PATH"] = "./module/simfang.ttf"
-# os.environ["FLAGS_allocator_strategy"] = "naive_best_fit"
-# os.environ["FLAGS_eager_delete_tensor_gb"] = "0"
-
 app = Flask(__name__)
 
-paddleocr = PaddleOCR(
-    use_doc_orientation_classify=False,
-    use_doc_unwarping=False,
-    use_textline_orientation=False,
-    text_detection_model_dir = "./module/PP-OCRv5_server_det",
-    text_recognition_model_dir = "./module/PP-OCRv5_server_rec"
-)
 
 def file_storage_to_ndarray(file_storage):
     file_storage.stream.seek(0)
@@ -36,18 +19,6 @@ def file_storage_to_ndarray(file_storage):
         img = img.convert('BGR')  # 统一维度为H×W×3
     return numpy.array(img)  # 自动生成dtype=uint8
 
-def print_order_no(result):
-    for res in result:
-        order_exist = False
-        for text in res['rec_texts']:
-            if "订单号" in text or "流水" in text:
-                app.logger.info(text)
-                order_exist=True
-        if not order_exist:
-            app.logger.info(res['rec_texts'])
-        app.logger.info("-------------------")
-
-
 
 # 定义路由和视图函数
 @app.route('/ocr', methods=['GET'])
@@ -55,21 +26,21 @@ def ocr():
     app.logger.info("开始")
     ### 使用url
     img_url = request.values.get('img_url')
+    result = ''
     if img_url is None:
-        filelist =request.files.getlist('img_file')
+        filelist = request.files.getlist('img_file')
         for file in filelist:
-            app.logger.info(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-            result = paddleocr.predict(input=file_storage_to_ndarray(file))
-            print_order_no(result)
+            app.logger.info('文件处理'+file.filename)
+            result = paddleocr.submit_ocr(input=file_storage_to_ndarray(file))
     else:
         app.logger.info(img_url)
-        result = paddleocr.predict(input=img_url)
-        print_order_no(result)
+        result = paddleocr.submit_ocr(input=img_url)
     return json.dumps({"text": result[0]['rec_texts'], "poly": [i.tolist() for i in result[0]['rec_polys']]}, indent=4,
                       ensure_ascii=False)
 
 
 # 启动应用
 if __name__ == '__main__':
+    paddleocr = PaddleOCRModelManager(app)
     app.logger.setLevel(logging.INFO)
     app.run(host="0.0.0.0", port=80)
---
+++ thread_single.py
@@ -0,0 +1,42 @@
+from paddleocr import PaddleOCR
+import os
+
+
+from concurrent.futures import ThreadPoolExecutor
+
+
+class PaddleOCRModelManager(ThreadPoolExecutor):
+
+    def __init__(self,current_app, **kwargs):
+        super(PaddleOCRModelManager, self).__init__(max_workers= 1,thread_name_prefix="test_",**kwargs)
+        os.environ["PADDLE_PDX_CACHE_HOME"] = "./module"
+        # os.environ["PADDLE_PDX_LOCAL_FONT_FILE_PATH"] = "./module/simfang.ttf"
+        # os.environ["FLAGS_allocator_strategy"] = "naive_best_fit"
+        # os.environ["FLAGS_eager_delete_tensor_gb"] = "0"
+        self.paddleocr = PaddleOCR(
+            use_doc_orientation_classify=False,
+            use_doc_unwarping=False,
+            use_textline_orientation=False,
+            text_detection_model_dir="./module/PP-OCRv5_server_det",
+            text_recognition_model_dir="./module/PP-OCRv5_server_rec"
+        )
+        self.app = current_app
+    def submit_ocr(self, **kwargs):
+        return self.submit(self.infer, **kwargs).result()
+
+    def infer(self, **kwargs):
+        result=self.paddleocr.predict(**kwargs)
+        self.print_order_no(result)
+        return result
+
+    def print_order_no(self,result):
+        for res in result:
+            order_exist = False
+            for text in res['rec_texts']:
+                if "订单号" in text or "流水" in text:
+                    self.app.logger.info(text)
+                    order_exist=True
+            if not order_exist:
+                self.app.logger.info(res['rec_texts'])
+            self.app.logger.info("-------------------")
+
